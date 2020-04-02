384a87ab00940b7fe0c508b040a22cc9579c9520
veryneatperson <62647942+veryneatperson@users.noreply.github.com>
2020-04-02 23:11:08 +0300

    Refactored getTicketsBySearchId() according to "no-continue" ESLint rule

--- src/store/sagas/tickets.js
+++ src/store/sagas/tickets.js
@@ -4,7 +4,7 @@ import { call, put, select } from 'redux-saga/effects';
 import {
   getTicketsRequest, getTicketsChunkSuccess, getTicketsSuccess, getTicketsFailure,
 } from '../actions/tickets';
-import { getSearchId } from './searchId';
+import getSearchId from './searchId';
 import { selectSearchId } from '../selectors/searchId';
 import { ticketsErrorMsg, baseTicketsURL } from '../../constants';
 
@@ -18,9 +18,7 @@ function* getTicketsBySearchId(action) {
       if (res.data.stop) shouldStop = true;
       yield put(getTicketsChunkSuccess(res.data.tickets));
     } catch (err) {
-      if (err.response.status === 500) {
-        continue;
-      } else {
+      if (err.response.status !== 500) {
         yield put(getTicketsFailure(ticketsErrorMsg));
         return;
       }
@@ -29,6 +27,7 @@ function* getTicketsBySearchId(action) {
   yield put(getTicketsSuccess());
 }
 
+// eslint-disable-next-line no-unused-vars
 export function* loadTicketsSaga(action) {
   yield call(getSearchId);
   const searchId = yield select(selectSearchId);
@@ -38,3 +37,5 @@ export function* loadTicketsSaga(action) {
     yield put(getTicketsFailure(ticketsErrorMsg));
   }
 }
+
+export default loadTicketsSaga;
