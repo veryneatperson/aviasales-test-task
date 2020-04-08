fe597648d1b5e16f6f8851bebe87917e6a3ca180
veryneatperson <62647942+veryneatperson@users.noreply.github.com>
2020-04-08 04:05:27 +0300

    Refactored <Tickets> according to changes in src/helpers

--- src/components/Ticket/Ticket.js
+++ src/components/Ticket/Ticket.js
@@ -4,9 +4,9 @@ import PropTypes from 'prop-types';
 import styles from './Ticket.module.scss';
 import {
   displayPrice,
-  displayStartAndEndDates,
+  displayTime,
   displayLayoversLabel,
-  displayDurationInHoursAndMinutes,
+  displayDuration,
 } from '../../helpers';
 import { carrierLogoURL } from '../../constants';
 
@@ -15,22 +15,30 @@ const Ticket = ({ ticket }) => {
   return (
     <div className={styles.Ticket}>
       <div className={styles.Header}>
-        <h3 className={styles.Price}>{displayPrice(price)} Р</h3>
-        <img src={`${carrierLogoURL}${carrier}.png`} alt="aviacompany logo.." />
+        <h3 className={styles.Price}>{displayPrice(price, 'Р')}</h3>
+        <img
+          className={styles.CarrierLogo}
+          src={`${carrierLogoURL}${carrier}.png`}
+          alt="aviacompany logo"
+        />
       </div>
       <div className={styles.Content}>
         {segments.map(({ origin, destination, date, stops, duration }) => (
           <div className={styles.ContentRow} key={`${date}${duration}`}>
             <div className={styles.Block}>
-              <span className={styles.Key}>{origin} - {destination}</span>
-              <span className={styles.Value}>{displayStartAndEndDates(date, duration)}</span>
+              <span className={styles.Key}>{`${origin} - ${destination}`}</span>
+              <span className={styles.Value}>
+                {displayTime(date, duration)}
+              </span>
             </div>
             <div className={styles.Block}>
               <span className={styles.Key}>в пути</span>
-              <span className={styles.Value}>{displayDurationInHoursAndMinutes(duration)}</span>
+              <span className={styles.Value}>{displayDuration(duration)}</span>
             </div>
             <div className={styles.Block}>
-              <span className={styles.Key}>{displayLayoversLabel(stops.length)}</span>
+              <span className={styles.Key}>
+                {displayLayoversLabel(stops.length)}
+              </span>
               <span className={styles.Value}>{stops.join(', ')}</span>
             </div>
           </div>
@@ -44,13 +52,15 @@ Ticket.propTypes = {
   ticket: PropTypes.shape({
     price: PropTypes.number.isRequired,
     carrier: PropTypes.string.isRequired,
-    segments: PropTypes.arrayOf(PropTypes.shape({
-      origin: PropTypes.string.isRequired,
-      destination: PropTypes.string.isRequired,
-      date: PropTypes.string.isRequired,
-      stops: PropTypes.arrayOf(PropTypes.string).isRequired,
-      duration: PropTypes.number.isRequired,
-    })),
+    segments: PropTypes.arrayOf(
+      PropTypes.shape({
+        origin: PropTypes.string.isRequired,
+        destination: PropTypes.string.isRequired,
+        date: PropTypes.string.isRequired,
+        stops: PropTypes.arrayOf(PropTypes.string).isRequired,
+        duration: PropTypes.number.isRequired,
+      }),
+    ),
   }),
 };
 
