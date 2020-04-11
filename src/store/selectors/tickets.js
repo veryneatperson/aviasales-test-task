2ff7cd186e4119e93ff03f7f04177ed8585b60ae
veryneatperson <62647942+veryneatperson@users.noreply.github.com>
2020-04-11 15:59:32 +0300

    Added new selector - selectTicketsLength

--- src/store/selectors/tickets.js
+++ src/store/selectors/tickets.js
@@ -1,3 +1,4 @@
 export const selectTickets = (state) => state.tickets.tickets;
 export const selectTicketsLoading = (state) => state.tickets.loading;
 export const selectTicketsErrorMsg = (state) => state.tickets.errorMsg;
+export const selectTicketsLength = (state) => state.tickets.tickets.length;
