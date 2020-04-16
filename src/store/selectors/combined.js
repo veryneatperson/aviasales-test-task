13aad8c42ae955fd906413feb0fbf41580d484eb
veryneatperson <62647942+veryneatperson@users.noreply.github.com>
2020-04-16 22:37:05 +0300

    Minor refactors in selectors/combined.js

--- src/store/selectors/combined.js
+++ src/store/selectors/combined.js
@@ -4,6 +4,8 @@ import { selectSearchIdLoading } from './searchId';
 import { selectTicketsLoading, selectTickets } from './tickets';
 import { selectFilters, selectSortBy } from './selection';
 
+const reduceBooleans = (...vals) => vals.reduce((acc, curr) => acc || curr);
+
 const filtersMapping = {
   any: null,
   noLayovers: 0,
@@ -14,30 +16,31 @@ const filtersMapping = {
 
 export const selectIsLoading = (state) => selectSearchIdLoading(state) || selectTicketsLoading(state);
 
-export const selectFilteredTickets = createSelector(
-  [selectFilters, selectTickets],
-  (filters, tickets) => {
-    if (filters.any) return tickets;
-    if ((filters.noLayovers + filters.oneLayover + filters.twoLayovers + filters.threeLayovers) === 0) return [];
-    const filtersArr = Object.entries(filters)
-      .filter((el) => el[1])
-      .map((el) => filtersMapping[el[0]]);
-    return tickets.filter(
-      (ticket) => filtersArr.includes(ticket.segments[0].stops.length)
-        || filtersArr.includes[ticket.segments[1].stops.length],
-    );
-  },
-);
+export const selectFilteredTickets = createSelector([selectFilters, selectTickets], (filters, tickets) => {
+  const { any, noLayovers, oneLayover, twoLayovers, threeLayovers } = filters;
+
+  if (any) return tickets;
+  if (!reduceBooleans(noLayovers, oneLayover, twoLayovers, threeLayovers)) return [];
+
+  const filtersArr = Object.entries(filters)
+    .filter((el) => el[1])
+    .map((el) => filtersMapping[el[0]]);
+
+  return tickets.filter(
+    (ticket) =>
+      filtersArr.includes(ticket.segments[0].stops.length) || filtersArr.includes[ticket.segments[1].stops.length],
+  );
+});
 
 export const selectFilteredAndSortedTickets = createSelector(
   [selectFilteredTickets, selectSortBy],
   (filteredTickets, sortBy) => {
-    switch (sortBy) {
-      case 'cheapest':
-        return [...filteredTickets].sort((a, b) => a.price - b.price);
-      case 'fastest':
-        return [...filteredTickets]
-          .sort((a, b) => (a.segments[0].duration + a.segments[1].duration) - (b.segments[0].duration + b.segments[1].duration));
+    if (sortBy === 'fastest') {
+      return [...filteredTickets].sort(
+        (a, b) => a.segments[0].duration + a.segments[1].duration - (b.segments[0].duration + b.segments[1].duration),
+      );
+    } else if (sortBy === 'cheapest') {
+      return [...filteredTickets].sort((a, b) => a.price - b.price);
     }
   },
 );
