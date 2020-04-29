42e2868754ea51009b313ba8660c3060068c8558
veryneatperson <62647942+veryneatperson@users.noreply.github.com>
2020-04-30 02:29:52 +0300

    Deleted unnecessary data in mockedData

--- src/fixtures/mockedData.js
+++ src/fixtures/mockedData.js
@@ -1,214 +1,204 @@
 const mockedTickets = [
   {
-    price: 60903,
-    carrier: 'S7',
-    segments: [
-      {
-        origin: 'MOW',
-        destination: 'HKT',
-        date: '2020-04-20T12:05:00.000Z',
-        stops: ['SHA'],
-        duration: 1378,
-      },
-      {
-        origin: 'MOW',
-        destination: 'HKT',
-        date: '2020-05-09T23:07:00.000Z',
-        stops: [],
-        duration: 1855,
-      },
-    ],
-  },
-  {
-    price: 47773,
-    carrier: 'FV',
-    segments: [
-      {
-        origin: 'MOW',
-        destination: 'HKT',
-        date: '2020-04-19T21:25:00.000Z',
-        stops: ['BKK', 'HKG'],
-        duration: 1035,
-      },
-      {
-        origin: 'MOW',
-        destination: 'HKT',
-        date: '2020-05-10T04:54:00.000Z',
-        stops: ['SIN', 'SHA', 'IST'],
-        duration: 1296,
-      },
-    ],
-  },
-  {
-    price: 98153,
-    carrier: 'EK',
-    segments: [
-      {
-        origin: 'MOW',
-        destination: 'HKT',
-        date: '2020-04-20T06:40:00.000Z',
-        stops: [],
-        duration: 1627,
-      },
-      {
-        origin: 'MOW',
-        destination: 'HKT',
-        date: '2020-05-09T21:08:00.000Z',
-        stops: ['HKG'],
-        duration: 1015,
-      },
-    ],
-  },
-  {
-    price: 74754,
-    carrier: 'FV',
-    segments: [
-      {
-        origin: 'MOW',
-        destination: 'HKT',
-        date: '2020-04-20T14:13:00.000Z',
-        stops: ['AUH', 'SHA'],
-        duration: 793,
-      },
-      {
-        origin: 'MOW',
-        destination: 'HKT',
-        date: '2020-05-10T11:35:00.000Z',
-        stops: ['KUL'],
-        duration: 1293,
-      },
-    ],
-  },
-  {
-    price: 89385,
-    carrier: 'FV',
-    segments: [
-      {
-        origin: 'MOW',
-        destination: 'HKT',
-        date: '2020-04-19T22:47:00.000Z',
-        stops: ['KUL', 'BKK', 'SIN'],
-        duration: 769,
-      },
-      {
-        origin: 'MOW',
-        destination: 'HKT',
-        date: '2020-05-10T01:55:00.000Z',
-        stops: ['IST', 'KUL', 'BKK'],
-        duration: 1864,
-      },
-    ],
-  },
-  {
-    price: 95173,
-    carrier: 'MH',
-    segments: [
-      {
-        origin: 'MOW',
-        destination: 'HKT',
-        date: '2020-04-20T07:18:00.000Z',
-        stops: ['IST'],
-        duration: 1542,
-      },
-      {
-        origin: 'MOW',
-        destination: 'HKT',
-        date: '2020-05-09T21:12:00.000Z',
-        stops: ['IST', 'SHA'],
-        duration: 1983,
-      },
-    ],
-  },
-  {
-    price: 94259,
-    carrier: 'MH',
-    segments: [
-      {
-        origin: 'MOW',
-        destination: 'HKT',
-        date: '2020-04-20T09:01:00.000Z',
-        stops: [],
-        duration: 1225,
-      },
-      {
-        origin: 'MOW',
-        destination: 'HKT',
-        date: '2020-05-10T17:13:00.000Z',
-        stops: ['BKK', 'AUH', 'SIN'],
-        duration: 1194,
-      },
-    ],
-  },
-  {
-    price: 37761,
-    carrier: 'FV',
-    segments: [
-      {
-        origin: 'MOW',
-        destination: 'HKT',
-        date: '2020-04-20T06:24:00.000Z',
-        stops: ['DXB', 'HKG', 'AUH'],
-        duration: 1394,
-      },
-      {
-        origin: 'MOW',
-        destination: 'HKT',
-        date: '2020-05-09T21:19:00.000Z',
-        stops: [],
-        duration: 1917,
-      },
-    ],
-  },
-  {
-    price: 34054,
+    price: 57394,
     carrier: 'TG',
     segments: [
       {
         origin: 'MOW',
         destination: 'HKT',
-        date: '2020-04-20T13:18:00.000Z',
+        date: '2020-05-07T00:08:00.000Z',
         stops: [],
-        duration: 1498,
+        duration: 1489,
       },
       {
         origin: 'MOW',
         destination: 'HKT',
-        date: '2020-05-09T23:09:00.000Z',
-        stops: ['KUL'],
-        duration: 733,
+        date: '2020-05-26T21:50:00.000Z',
+        stops: ['IST', 'SHA', 'SIN'],
+        duration: 807,
       },
     ],
   },
   {
-    price: 29241,
+    price: 85430,
+    carrier: 'MH',
+    segments: [
+      {
+        origin: 'MOW',
+        destination: 'HKT',
+        date: '2020-05-07T07:54:00.000Z',
+        stops: ['DXB', 'KUL'],
+        duration: 1356,
+      },
+      {
+        origin: 'MOW',
+        destination: 'HKT',
+        date: '2020-05-27T07:35:00.000Z',
+        stops: [],
+        duration: 1492,
+      },
+    ],
+  },
+  {
+    price: 86090,
     carrier: 'EY',
     segments: [
       {
         origin: 'MOW',
         destination: 'HKT',
-        date: '2020-04-20T17:10:00.000Z',
-        stops: ['DXB', 'HKG'],
-        duration: 1587,
+        date: '2020-05-07T16:11:00.000Z',
+        stops: ['KUL'],
+        duration: 1579,
       },
       {
         origin: 'MOW',
         destination: 'HKT',
-        date: '2020-05-10T08:23:00.000Z',
-        stops: ['DXB', 'SIN'],
-        duration: 1237,
+        date: '2020-05-27T14:35:00.000Z',
+        stops: ['HKG', 'IST', 'SIN'],
+        duration: 1732,
+      },
+    ],
+  },
+  {
+    price: 51976,
+    carrier: 'S7',
+    segments: [
+      {
+        origin: 'MOW',
+        destination: 'HKT',
+        date: '2020-05-07T03:54:00.000Z',
+        stops: ['HKG', 'SHA', 'AUH'],
+        duration: 1017,
+      },
+      {
+        origin: 'MOW',
+        destination: 'HKT',
+        date: '2020-05-27T09:18:00.000Z',
+        stops: ['SIN'],
+        duration: 905,
+      },
+    ],
+  },
+  {
+    price: 86071,
+    carrier: 'EK',
+    segments: [
+      {
+        origin: 'MOW',
+        destination: 'HKT',
+        date: '2020-05-07T00:21:00.000Z',
+        stops: ['IST', 'KUL', 'BKK'],
+        duration: 1498,
+      },
+      {
+        origin: 'MOW',
+        destination: 'HKT',
+        date: '2020-05-27T00:42:00.000Z',
+        stops: [],
+        duration: 1267,
+      },
+    ],
+  },
+  {
+    price: 71215,
+    carrier: 'FV',
+    segments: [
+      {
+        origin: 'MOW',
+        destination: 'HKT',
+        date: '2020-05-07T14:05:00.000Z',
+        stops: ['AUH', 'IST', 'SIN'],
+        duration: 1345,
+      },
+      {
+        origin: 'MOW',
+        destination: 'HKT',
+        date: '2020-05-27T05:43:00.000Z',
+        stops: [],
+        duration: 1775,
+      },
+    ],
+  },
+  {
+    price: 73779,
+    carrier: 'S7',
+    segments: [
+      {
+        origin: 'MOW',
+        destination: 'HKT',
+        date: '2020-05-07T03:53:00.000Z',
+        stops: ['HKG', 'BKK', 'SIN'],
+        duration: 911,
+      },
+      {
+        origin: 'MOW',
+        destination: 'HKT',
+        date: '2020-05-27T17:23:00.000Z',
+        stops: ['AUH', 'DXB', 'SIN'],
+        duration: 734,
+      },
+    ],
+  },
+  {
+    price: 44731,
+    carrier: 'S7',
+    segments: [
+      {
+        origin: 'MOW',
+        destination: 'HKT',
+        date: '2020-05-07T15:54:00.000Z',
+        stops: ['SHA', 'KUL', 'IST'],
+        duration: 1057,
+      },
+      {
+        origin: 'MOW',
+        destination: 'HKT',
+        date: '2020-05-27T13:08:00.000Z',
+        stops: ['HKG', 'DXB'],
+        duration: 939,
+      },
+    ],
+  },
+  {
+    price: 18473,
+    carrier: 'EK',
+    segments: [
+      {
+        origin: 'MOW',
+        destination: 'HKT',
+        date: '2020-05-07T00:18:00.000Z',
+        stops: ['SHA', 'HKG'],
+        duration: 1890,
+      },
+      {
+        origin: 'MOW',
+        destination: 'HKT',
+        date: '2020-05-27T12:32:00.000Z',
+        stops: ['IST'],
+        duration: 1428,
+      },
+    ],
+  },
+  {
+    price: 99283,
+    carrier: 'MH',
+    segments: [
+      {
+        origin: 'MOW',
+        destination: 'HKT',
+        date: '2020-05-07T00:52:00.000Z',
+        stops: [],
+        duration: 938,
+      },
+      {
+        origin: 'MOW',
+        destination: 'HKT',
+        date: '2020-05-27T10:03:00.000Z',
+        stops: ['AUH', 'IST', 'KUL'],
+        duration: 1284,
       },
     ],
   },
 ];
 
-const responseUnfinished = {
-  tickets: mockedTickets.slice(0, 5),
-  stop: false,
-};
-
-const responseFinished = {
-  tickets: mockedTickets.slice(5),
-  stop: false,
-};
-
-export { mockedTickets, responseUnfinished, responseFinished };
+export { mockedTickets };
